9d2fb75f25bca8f884a88cb56f9cdc138cfe4745
Revanth_Ctrl <revanth_ctrl@juhackathon.org>
2019-08-30 02:28:10 +0530

    Final commit
    Everything works properly.
    All the HTML, CSS, JS works without any problems.
    The sizes very with respect to screen size.
    
    Day 1 of Code Challenge.

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ function play(e)
     restart.style.display = 'inline-block';
     const playerChoice = e.target.id;
     const computerChoice = getComputerChoice();
-
     const winner = getWinner(playerChoice,computerChoice);
+
+    showWinner(winner,computerChoice);
 }
 
 // Getting the computers choice
@@ -40,6 +41,7 @@ function getComputerChoice()
 }
 
 //Selecting the winner
+
 function getWinner(p,c)
 {
     if(p === c)
@@ -74,8 +76,87 @@ function getWinner(p,c)
 
 }
 
+function showWinner(winner,computerChoice)
+{
+    if(winner === 'player')
+    {
+        //Incrimenting player score 
+        scoreboard.player++;
+
+        //show result
+        result.innerHTML = `
+                            <h1 class="text-win">You Win</h1>
+                            <i class ="far fa-hand-${computerChoice} fa-10x"></i>
+                            <p>Computer chose <strong>${computerChoice}</strong></p>
+                             `;   
+    }
+    else if(winner === 'computer')
+    {
+        //Incrimenting computer score 
+        scoreboard.computer++;
+
+        //show result
+        result.innerHTML = `
+                            <h1 class="text-lose">You Lose</h1>
+                            <i class ="far fa-hand-${computerChoice} fa-10x"></i>
+                            <p>Computer chose <strong>${computerChoice}</strong></p>
+                             `; 
+    }
+    else
+    {
+        result.innerHTML = `
+                            <h1 class="draw">DRAW</h1>
+                            <i class ="far fa-hand-${computerChoice} fa-10x"></i>
+                            <p>Computer chose <strong>${computerChoice}</strong></p>
+                             `; 
+    }
+
+    //show score
+    score.innerHTML = `
+                        <p>Player : ${scoreboard.player}</p>
+                        <p>Computer : ${scoreboard.computer}</p>
+                      `;
+
+     //Bringing the resul;t front            
+      modal.style.display = 'block';
+   
+}
+
+//clear screen to take next turn
+
+function clearModal(e)
+{
+    if(e.target === modal)
+    {
+        modal.style.display = 'none';
+    }
+}
+
+//to restart the  game
+
+function restartGame()
+{
+    scoreboard.player = 0;
+    scoreboard.computer = 0;
+    score.innerHTML = `
+                        <p>Player : 0</p>
+                        <p>Computer : 0</p>
+    
+                        `;
+
+}
+
+
 // Event Listners
  
 choices.forEach(choice => choice.addEventListener('click',play));
+
+//clear the window on click
+
+window.addEventListener('click',clearModal);
+
+//to restart the game
+
+restart.addEventListener('click',restartGame);
 
 
